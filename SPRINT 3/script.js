c3c43fd273bef69c2dddeb17dee237356009fa37
Tawan <josetawan300@gmail.com>
2022-07-29 17:32:56 -0300

    entrega

---
+++ SPRINT 3/script.js
@@ -0,0 +1,7 @@
+let nome= "Tawan"
+let sobrenome="ALencar"
+let anoNascimento=1999
+let cidade="Teresina-PI"
+let idade= 2022-anoNascimento
+let sobremim= `Eu sou ${nome} ${sobrenome}, tenho ${idade} anos, nasci na cidade de ${cidade}`
+console.log(sobremim)
